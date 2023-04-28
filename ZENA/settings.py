54c60587c621e17ad09feb0125da0c806d2a2b77
enockabere <maebaenock95@gmail.com>
2023-04-28 17:50:50 +0300

    create register serializers

--- ZENA/settings.py
+++ ZENA/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['.localhost','.127.0.0.1']
 
-
+AUTH_USER_MODEL = 'authentication.User'
 # Application definition
 
 INSTALLED_APPS = [
